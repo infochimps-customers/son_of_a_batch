57be309d34a14358309a5961b2ccf2f13326c033
Philip (flip) Kromer <flip@infochimps.org>
2011-07-24 02:08:53 -0500

    lightweight tidying up: .gitignore, better spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,29 @@
-require 'rspec'
 require 'spork'
-require 'bundler'
-$:<< '../lib' << 'lib' << 'vendor/goliath/lib'
+require 'rspec'
 
 Spork.prefork do
-  Bundler.setup
-  Bundler.require
+  # This code is run only once when the spork server is started
 
+  ENV["RACK_ENV"] ||= 'test'
+  RACK_ENV = ENV["RACK_ENV"] unless defined?(RACK_ENV)
+
+  require File.join(File.dirname(__FILE__), '../lib/boot')
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+  require 'goliath'
+  require 'em-synchrony'
   require 'goliath/test_helper'
+  require 'gorillib/hash/reverse_merge'
 
-  ::RSpec.configure do |c|
-    c.include Goliath::TestHelper, :example_group => {
-      :file_path => /spec\/integration/
+  Settings ||= YAML.load(File.open(Goliath.root_path("config/macaque.yaml")))
+
+  # Requires custom matchers & macros, etc from files in ./support/ & subdirs
+  Dir[Goliath.root_path("spec/support/**/*.rb")].each {|f| require f}
+
+  # Configure rspec
+  RSpec.configure do |config|
+    config.include Goliath::TestHelper, :example_group => {
+      :file_path => %r{spec/integration}
     }
   end
 end
@@ -19,7 +31,3 @@ end
 Spork.each_run do
   # This code will be run each time you run your specs.
 end
-
-
-
-
